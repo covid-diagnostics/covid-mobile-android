8ee7322fab50e7855a73af5765c9da48e9145b2f
rotemsamuel <rotemsamuel@campus.technion.ac.il>
2020-03-24 20:57:45 +0200

    added VideoRecorderActivity

--- app/src/main/java/com/example/coronadiagnosticapp/ui/fragments/camera/CameraFragment.kt
+++ app/src/main/java/com/example/coronadiagnosticapp/ui/fragments/camera/CameraFragment.kt
@@ -2,14 +2,16 @@ package com.example.coronadiagnosticapp.ui.fragments.camera
 
 import android.Manifest
 import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.coronadiagnosticapp.R
+import com.example.coronadiagnosticapp.ui.activities.VideoRecordActivity
 import com.example.coronadiagnosticapp.ui.fragments.ScopedFragment
 import kotlinx.android.synthetic.main.camera_fragment.*
 import javax.inject.Inject
@@ -30,7 +32,8 @@ private val REQUIRED_PERMISSIONS = arrayOf(
 
 class CameraFragment : ScopedFragment() {
 
-    @Inject lateinit var viewModel: CameraViewModel
+    @Inject
+    lateinit var viewModel: CameraViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +46,52 @@ class CameraFragment : ScopedFragment() {
         super.onActivityCreated(savedInstanceState)
 
         startCamera.setOnClickListener {
-            // Ask permission to use the camera
+            // TODO: Ask permission to use the camera
+            if (context?.let { it1 ->
+                    ActivityCompat.checkSelfPermission(
+                        it1,
+                        Manifest.permission.CAMERA
+                    )
+                } != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                activity?.let { it1 ->
+                    ActivityCompat.requestPermissions(
+                        it1, arrayOf(
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ), 200
+                    )
+                }
+            } else {
+                activity?.let {
+                    val intent = Intent(it, VideoRecordActivity::class.java)
+                    it.startActivity(intent)
+                }
+            }
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 200) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                activity?.let {
+                    val intent = Intent(it, VideoRecordActivity::class.java)
+                    it.startActivity(intent)
+                }
+            } else {
+                Toast.makeText(context, "cannot continue without permissions", Toast.LENGTH_LONG)
+                    .show()
+                activity?.finish()//FIXME not sure if correct
+            }
         }
     }
 
