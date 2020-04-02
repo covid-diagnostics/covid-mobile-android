cd79b7dc7abf4576ef8482d7ddf816492039eab7
Yarin Azoulay <yarinaz17@gmail.com>
2020-04-02 14:30:47 +0300

    Code refactor

--- app/src/main/java/com/example/coronadiagnosticapp/ui/activities/OxymeterActivity.java
+++ app/src/main/java/com/example/coronadiagnosticapp/ui/activities/OxymeterActivity.java
@@ -45,57 +45,37 @@ public class OxymeterActivity extends Activity {
     // Variables Initialization
     private static final String TAG = "HeartRateMonitor";
     private static final AtomicBoolean processing = new AtomicBoolean(false);
-    private SurfaceView preview = null;
     private static SurfaceHolder previewHolder = null;
     private static Camera camera = null;
-
-    //Initialize an object that calculates the rolling average of last 15 samples
-    private SMA calc_mov_avg = new SMA(15);
-
-    //Button
-    private Button readyBtn;
-
-    //TextView
-    private TextView alert;
-
-    //ProgressBar
-    ProgressBar progressBarView;
-    TextView tv_time;
-    int progress;
-    CountDownTimer countDownTimer;
-    int endTime = 250;
-
     //Freq + timer variable
     private static long startTime = 0;
-    private double SamplingFreq;
-
     // SPO2 variables
     private static double RedBlueRatio = 0;
-    double Stdr = 0;
-    double Stdb = 0;
-    double sumred = 0;
-    double sumblue = 0;
-    double sumgreen = 0;
     public int o2;
-
-    RotateAnimation makeVertical;
-
     //RespirationRate variable
     public int Breath = 0;
     public double bufferAvgBr = 0;
-
     // Heart Rate vaiables
     public double bufferAvgB = 0;
     public int Beats = 0;
     public double peakBpm = 0;
-
     //Arraylist
     public ArrayList<Double> RedAvgList = new ArrayList<Double>();
     public ArrayList<Double> BlueAvgList = new ArrayList<Double>();
     public ArrayList<Double> GreenAvgList = new ArrayList<Double>();
     public ArrayList<Double> MovAvgRed = new ArrayList<Double>();
     public int counter = 0;
-
+    //ProgressBar
+    ProgressBar progressBarView;
+    TextView tv_time;
+    int progress;
+    CountDownTimer countDownTimer;
+    int endTime = 250;
+    double Stdr = 0;
+    double Stdb = 0;
+    double sumred = 0;
+    double sumblue = 0;
+    RotateAnimation makeVertical;
     //CSV Writing
     String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
     String fileName = "AnalysisData.csv";
@@ -103,132 +83,14 @@ public class OxymeterActivity extends Activity {
     String filePath = baseDir + File.separator + fileName;
     File f = new File(filePath);
     CSVWriter writer;
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_record);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        // XML - Java Connecting
-        preview = (SurfaceView) findViewById(R.id.preview);
-        alert = (TextView) findViewById(R.id.putfinger);
-        previewHolder = preview.getHolder();
-        previewHolder.addCallback(surfaceCallback);
-        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        readyBtn = (Button) findViewById(R.id.ready_btn);
-        progressBarView = (ProgressBar) findViewById(R.id.barTimer);
-        tv_time = (TextView) findViewById(R.id.textTimer);
-
-
-
-        /*Animation*/
-        makeVertical = new RotateAnimation(0, -90, RELATIVE_TO_SELF, 0.5f, RELATIVE_TO_SELF, 0.5f);
-        makeVertical.setFillAfter(true);
-        progressBarView.startAnimation(makeVertical);
-        progressBarView.setSecondaryProgress(endTime);
-        progressBarView.setProgress(0);
-
-        readyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fn_countdown();
-                startTime = System.currentTimeMillis();
-                Log.e(TAG, "Time has started = " + Long.toString(startTime));
-            }
-        });
-
-
-    }
-
-    private void fn_countdown() {
-
-
-        try {
-            countDownTimer.cancel();
-
-        } catch (Exception e) {
-
-        }
-
-        progress = 1;
-        endTime = 30;
-
-        countDownTimer = new CountDownTimer(endTime * 1000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                setProgress(progress, endTime);
-                progress = progress + 1;
-                Log.e(TAG, "ticking progress = " + progress);
-                int seconds = (int) (millisUntilFinished / 1000) % 60;
-                String newtime = seconds + " seconds";
-
-                if (newtime.equals("0")) {
-                    tv_time.setText("Press start");
-                } else {
-                    tv_time.setText(seconds + " seconds");
-                }
-
-            }
-
-            @Override
-            public void onFinish() {
-                progress = 0;
-                setProgress(progress, 30);
-            }
-        };
-        countDownTimer.start();
-
-
-    }
-
-    public void setProgress(int startTime, int endTime) {
-        progressBarView.setMax(endTime);
-        progressBarView.setSecondaryProgress(endTime);
-        progressBarView.setProgress(startTime);
-
-    }
-
-    //Prevent the system from restarting your activity during certain configuration changes,
-    // but receive a callback when the configurations do change, so that you can manually update your activity as necessary.
-    //such as screen orientation, keyboard availability, and language
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
-
-    //Wakelock + Open device camera + set orientation to 90 degree
-    //store system time as a start time for the analyzing process
-    //your activity to start interacting with the user.
-    // This is a good place to begin animations, open exclusive-access devices (such as the camera)
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        camera = Camera.open();
-
-        camera.setDisplayOrientation(90);
-
-        Log.e("OnResume():", "Called.");
-    }
-
-    //call back the frames then release the camera + wakelock and Initialize the camera to null
-    //Called as part of the activity lifecycle when an activity is going into the background, but has not (yet) been killed. The counterpart to onResume().
-    //When activity B is launched in front of activity A,
-    // this callback will be invoked on A. B will not be created until A's onPause() returns, so be sure to not do anything lengthy here.
-    @Override
-    public void onPause() {
-        super.onPause();
-        camera.setPreviewCallback(null);
-        camera.stopPreview();
-        camera.release();
-        camera = null;
-    }
-
+    private SurfaceView preview = null;
+    //Initialize an object that calculates the rolling average of last 15 samples
+    private SMA calc_mov_avg = new SMA(15);
+    //Button
+    private Button readyBtn;
+    //TextView
+    private TextView alert;
+    private double SamplingFreq;
     //getting frames data from the camera and start the heartbeat process
     private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
 
@@ -260,7 +122,6 @@ public class OxymeterActivity extends Activity {
             BlueAvg = ImageProcessing.decodeYUV420SPtoRedBlueGreenAvg(data.clone(), height, width, 2); //1 stands for red intensity, 2 for blue, 3 for green
             sumblue = sumblue + BlueAvg;
             GreenAvg = ImageProcessing.decodeYUV420SPtoRedBlueGreenAvg(data.clone(), height, width, 3);
-            sumgreen = sumgreen + GreenAvg;
 
             RedAvgList.add(RedAvg);
             BlueAvgList.add(BlueAvg);
@@ -268,56 +129,28 @@ public class OxymeterActivity extends Activity {
 
             //To check if we got a good red intensity to process if not return to the condition and set it again until we get a good red intensity
             if (RedAvg < 200) {
-                alert.setVisibility(View.VISIBLE); // Make alert no finger - visible
-                sumred = 0;
-                sumblue = 0;
-                RedAvgList.clear();
-                BlueAvgList.clear();
-                counter = 0;
+                resetProcessing();
+                removeProgressBarAndShowAlert();
                 processing.set(false);
-                countDownTimer.cancel();
-                setProgress(0, 30);
-                progress = 0;
-                fn_countdown();
-                if (progressBarView.getVisibility() == View.VISIBLE) {
-                    progressBarView.clearAnimation();
-                    progressBarView.setVisibility(View.GONE);
-                }
-                startTime = System.currentTimeMillis();
                 return;
             } else {
-                alert.setVisibility(View.INVISIBLE);
-                if (!(progressBarView.getVisibility() == View.VISIBLE)) {
-                    progressBarView.startAnimation(makeVertical);
-                    progressBarView.setVisibility(View.VISIBLE);
-                }
-
-                progressBarView.setVisibility(View.VISIBLE);
+                showProgressBarAndHideAlert();
             }
-            if (counter == 0)
-                Log.e(TAG, "First count of counter, time =" + Long.toString(startTime));
-
 
             ++counter; //countes number of frames in 30 seconds
-            Log.e(TAG, "Counter is at = " + counter);
 
-            long endTime = System.currentTimeMillis();
+            long endTime = System.currentTimeMillis(); // Set an endTime each frame to check exactly when the timer reach 30 secondes
             double totalTimeInSecs = (endTime - startTime) / 1000d; //to convert time to seconds
             if (totalTimeInSecs >= 30 && startTime != 0) { //when 30 seconds of measuring passes do the following " we chose 30 seconds to take half sample since 60 seconds is normally a full sample of the heart beat
-                Log.e(TAG, "30 secondes passed ! totalTimeInSecs is now = " + Double.toString(totalTimeInSecs));
-                Log.e(TAG, "The sampling frequent is = " + Double.toString(counter / totalTimeInSecs));
                 double avg_hr = (sumred) / (RedAvgList.size());
                 List<String[]> rows = new ArrayList<String[]>(); // Initializing List of string array's to generate CSV file
                 rows.add(HEADER); // Adding Header to the CSV file
-                Log.e(TAG, "The average red is = " + avg_hr);
                 for (int i = 0; i < RedAvgList.size(); i++) {
                     if (i < 15) {                                   //Assign the average red received to the first 15 samples
-                        calc_mov_avg.compute(avg_hr);           //Add the value to the moving average object
-                        //  r.add(avg_hr);
-                        MovAvgRed.add(i, avg_hr);               //Add the value to the MobAvgRed list
+                        calc_mov_avg.compute(avg_hr);               //Add the value to the moving average object
+                        MovAvgRed.add(i, avg_hr);                   //Add the value to the MobAvgRed list
                         rows.add(new String[]{Integer.toString(i), RedAvgList.get(i).toString(), Double.toString(avg_hr)});       ///Add to CSV
                     } else {
-                        // r.add(RedAvgList.get(i));
                         MovAvgRed.add(calc_mov_avg.compute(RedAvgList.get(i)));
                         Log.e(TAG, "Current Average = " + calc_mov_avg.currentAverage());
                         rows.add(new String[]{Integer.toString(i), RedAvgList.get(i).toString(), Double.toString(calc_mov_avg.currentAverage())});
@@ -325,13 +158,7 @@ public class OxymeterActivity extends Activity {
 
                 }
 //--------------------------CSV Writing-------------------------------
-                try {
-                    writer = new CSVWriter(new FileWriter(filePath));
-                    writer.writeAll(rows);
-                    writer.close();
-                } catch (IOException ex) {
-                    Log.e(TAG, "FileWriter Exception" + ex);
-                }
+                writeCSV(rows);
 //--------------------------------------------------------------------
                 caclualteMaBPM(totalTimeInSecs);
 
@@ -398,27 +225,16 @@ public class OxymeterActivity extends Activity {
 
 //-----------------Measurement failed, doing start-over by setting counter to 0 and setting startTime to current---------------------------------------------//
                 if ((o2 < 80 || o2 > 99) || ((bufferAvgB < 45 || bufferAvgB > 200) && (peakBpm < 45 || peakBpm > 200))) {
-                    Log.e("O2 Value before Toast = ", "" + Integer.toString(o2));
-                    Toast.makeText(getApplicationContext(), "Measurement Failed, Please start the button again when you are ready !", Toast.LENGTH_SHORT).show();
-                    sumred = 0;
-                    sumblue = 0;
-                    RedAvgList.clear();
-                    BlueAvgList.clear();
-                    counter = 0;
+                    Toast.makeText(getApplicationContext(), "Measurement Failed, Please press the start button again when you are ready !", Toast.LENGTH_LONG).show();
+                    resetProcessing();
                     o2 = 0;
+                    startTime = 0; //Re-assign startTime to 0, because the resetProcessing() sets it to current time and we want to stop the process until user press the start button.
                     processing.set(false);
-                    countDownTimer.cancel();
-                    progress = 0;
-                    setProgress(0, 30);
-                    startTime = 0;
                     return;
                 }
                 Beats = (int) bufferAvgB;
                 Breath = (int) bufferAvgBr;
 
-
-                camera.setPreviewCallback(null);
-                camera.stopPreview();
             }
 
 
@@ -495,7 +311,6 @@ public class OxymeterActivity extends Activity {
             peakBpm = 60000 / (avgRR_List);
         }
     };
-
     private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
 
 
@@ -553,8 +368,168 @@ public class OxymeterActivity extends Activity {
         return result;
     }
 
+    //Prevent the system from restarting your activity during certain configuration changes,
+    // but receive a callback when the configurations do change, so that you can manually update your activity as necessary.
+    //such as screen orientation, keyboard availability, and language
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_record);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // XML - Java Connecting
+        preview = (SurfaceView) findViewById(R.id.preview);
+        alert = (TextView) findViewById(R.id.putfinger);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        readyBtn = (Button) findViewById(R.id.ready_btn);
+        progressBarView = (ProgressBar) findViewById(R.id.barTimer);
+        tv_time = (TextView) findViewById(R.id.textTimer);
+
+
+
+        /*Animation*/
+        makeVertical = new RotateAnimation(0, -90, RELATIVE_TO_SELF, 0.5f, RELATIVE_TO_SELF, 0.5f);
+        makeVertical.setFillAfter(true);
+        progressBarView.startAnimation(makeVertical);
+        progressBarView.setSecondaryProgress(endTime);
+        progressBarView.setProgress(0);
+
+        readyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fn_countdown();
+                startTime = System.currentTimeMillis();
+                Log.e(TAG, "Time has started = " + Long.toString(startTime));
+            }
+        });
+
+
+    }
+
+    private void fn_countdown() {
+
+        try {
+            countDownTimer.cancel();
+
+        } catch (Exception e) {
+
+        }
+
+        progress = 1;
+        endTime = 30;
+
+        countDownTimer = new CountDownTimer(endTime * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                setProgress(progress, endTime);
+                progress = progress + 1;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                String newtime = seconds + " seconds";
+
+                if (newtime.equals("0")) {
+                    tv_time.setText("Press start");
+                } else {
+                    tv_time.setText(seconds + " seconds");
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+                progress = 0;
+                setProgress(progress, 30);
+            }
+        };
+        countDownTimer.start();
+
+
+    }
+
+    public void setProgress(int startTime, int endTime) {
+        progressBarView.setMax(endTime);
+        progressBarView.setSecondaryProgress(endTime);
+        progressBarView.setProgress(startTime);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    //Wakelock + Open device camera + set orientation to 90 degree
+    //store system time as a start time for the analyzing process
+    //your activity to start interacting with the user.
+    // This is a good place to begin animations, open exclusive-access devices (such as the camera)
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+
+    }
+
+    //call back the frames then release the camera + wakelock and Initialize the camera to null
+    //Called as part of the activity lifecycle when an activity is going into the background, but has not (yet) been killed. The counterpart to onResume().
+    //When activity B is launched in front of activity A,
+    // this callback will be invoked on A. B will not be created until A's onPause() returns, so be sure to not do anything lengthy here.
+    @Override
+    public void onPause() {
+        super.onPause();
+        camera.setPreviewCallback(null);
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+    private void resetProcessing() {
+        sumred = 0;
+        sumblue = 0;
+        RedAvgList.clear();
+        BlueAvgList.clear();
+        counter = 0;
+        countDownTimer.cancel();
+        setProgress(0, 30);
+        progress = 0;
+        fn_countdown();
+        startTime = System.currentTimeMillis();
+    }
+
+    private void writeCSV(List<String[]> data) {
+        try {
+            writer = new CSVWriter(new FileWriter(filePath));
+            writer.writeAll(data);
+            writer.close();
+        } catch (IOException ex) {
+            Log.e(TAG, "FileWriter Exception: " + ex);
+        }
+    }
+
+    private void removeProgressBarAndShowAlert() {
+        alert.setVisibility(View.VISIBLE); // Make alert "no finger" - visible
+        if (progressBarView.getVisibility() == View.VISIBLE) {
+            progressBarView.clearAnimation();
+            progressBarView.setVisibility(View.GONE);
+        }
+    }
+
+    private void showProgressBarAndHideAlert() {
+        alert.setVisibility(View.INVISIBLE);
+        if (!(progressBarView.getVisibility() == View.VISIBLE)) {
+            progressBarView.startAnimation(makeVertical);
+            progressBarView.setVisibility(View.VISIBLE);
+        }
+        progressBarView.setVisibility(View.VISIBLE);
+    }
 }
+
