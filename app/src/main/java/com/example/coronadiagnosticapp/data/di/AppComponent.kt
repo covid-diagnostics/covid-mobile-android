1a3427a1148a51038020524828108e3684dce886
itaistopel <itaistopel@gmail.com>
2020-04-27 14:15:21 +0300

    Added fragment

--- app/src/main/java/com/example/coronadiagnosticapp/data/di/AppComponent.kt
+++ app/src/main/java/com/example/coronadiagnosticapp/data/di/AppComponent.kt
@@ -16,6 +16,7 @@ import com.example.coronadiagnosticapp.ui.fragments.dailtyMetric.DailyMetricFrag
 import com.example.coronadiagnosticapp.ui.fragments.information.InformationFragment
 import com.example.coronadiagnosticapp.ui.fragments.instruction.InstructionsFragment
 import com.example.coronadiagnosticapp.ui.fragments.recorder.RecorderFragment
+import com.example.coronadiagnosticapp.ui.fragments.recorder.RecorderFragment2
 import com.example.coronadiagnosticapp.ui.fragments.register.RegisterFragment
 import com.example.coronadiagnosticapp.ui.fragments.resultFragment.ResultFragment
 import com.example.coronadiagnosticapp.ui.fragments.welcome.WelcomeFragment
@@ -34,6 +35,7 @@ interface AppComponent {
     fun inject(fragment: DailyMetricFragment)
     fun inject(fragment: ResultFragment)
     fun inject(fragment: RecorderFragment)
+    fun inject(fragment: RecorderFragment2)
     fun inject(fragment: CameraFragment)
     fun inject(fragment: WelcomeFragment)
     fun inject(fragment: InstructionsFragment)
