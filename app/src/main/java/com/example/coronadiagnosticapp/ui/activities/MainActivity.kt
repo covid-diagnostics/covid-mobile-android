cbaf3c60cf4bb2e33e0f64b95aef57d0270f2fd4
ilaym007 <ilaym007@gmail.com>
2020-05-05 11:24:02 +0300

    oops

--- app/src/main/java/com/example/coronadiagnosticapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/coronadiagnosticapp/ui/activities/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.example.coronadiagnosticapp.ui.activities
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import com.afollestad.vvalidator.util.hide
 import com.afollestad.vvalidator.util.show
 import com.example.coronadiagnosticapp.R
 import com.example.coronadiagnosticapp.ui.activities.Reminder.RegisterNotificationService
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val languageToLoad = "iw" // your language
-//
-//        val locale = Locale(languageToLoad)
-//        Locale.setDefault(locale)
-//        val config = Configuration()
-//        config.locale = locale
-//        baseContext.resources.updateConfiguration(
-//            config,
-//            baseContext.resources.displayMetrics
-//        )
-        val i = Intent(applicationContext,  RegisterNotificationService::class.java)
+        val i = Intent(applicationContext, RegisterNotificationService::class.java)
         startService(i)
 
         setContentView(R.layout.activity_main)
