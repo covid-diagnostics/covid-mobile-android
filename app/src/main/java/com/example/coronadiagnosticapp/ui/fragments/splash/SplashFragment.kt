5d9d2bb8683d7f50c2467900e483ee6fd669724f
eranCat <eranka12@gmail.com>
2020-05-11 13:01:00 +0300

    Added logo animation

--- app/src/main/java/com/example/coronadiagnosticapp/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/example/coronadiagnosticapp/ui/fragments/splash/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.example.coronadiagnosticapp.ui.fragments.splash
 
+import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.coronadiagnosticapp.R
 import com.example.coronadiagnosticapp.utils.getAppComponent
+import kotlinx.android.synthetic.main.fragment_splash.*
 import javax.inject.Inject
 
-class SplashFragment : Fragment() {
+class SplashFragment : Fragment(), Animator.AnimatorListener {
 
     @Inject
     lateinit var viewModel: SplashViewModel
@@ -27,7 +29,23 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        animateLogo()
+    }
 
+    private fun animateLogo() = with(logo_img) {
+        alpha = 0.5f
+        scaleX = 0f
+        scaleY = 0f
+        animate()
+            .setDuration(500)
+            .setListener(this@SplashFragment)
+            .alpha(1f)
+            .scaleX(1f)
+            .scaleY(1f)
+            .start()
+    }
+
+    private fun moveToNextScreen() {
         val isLoggedIn = viewModel.isLoggedIn()
         viewModel.setIsFirstTime(!isLoggedIn)
         val id = if (isLoggedIn) {
@@ -37,4 +55,14 @@ class SplashFragment : Fragment() {
         }
         findNavController().navigate(id)
     }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        moveToNextScreen()
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {}
+
+    override fun onAnimationCancel(animation: Animator?) {}
+
+    override fun onAnimationStart(animation: Animator?) {}
 }
